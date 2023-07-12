f44b77e5f816055685f7d8ba13781605c4d708e2
Asharib <asharibarain4@gmail.com>
2023-07-12 18:29:01 +0500

    Update: change the product card

--- components/AllProductsCard.tsx
+++ components/AllProductsCard.tsx
@@ -42,23 +42,6 @@ const AllProductsCard = ({ data }: { data: IProduct[] }) => {
 
   return (
     <div className="max-w-screen-lg justify-between py-2 my-16 mx-auto">
-      <div className="mb-4 ">
-        <select
-          id="pricing"
-          onChange={(e) => setSelectedValue(e.target.value)}
-          className=" lg:mx-0 cursor-pointer mx-auto max-w-xs py-3 px-4 pr-9 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 "
-        >
-          <option defaultValue="Select sorting type" className="p-4">
-            Select sorting type
-          </option>
-          <option value="low" className="p-4">
-            Price low to high
-          </option>
-          <option value="high" className="p-4">
-            Price high to low
-          </option>
-        </select>
-      </div>
       <Suspense fallback={<Loader />}>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center gap-2">
           {filteredData &&
@@ -73,15 +56,14 @@ const AllProductsCard = ({ data }: { data: IProduct[] }) => {
                 // }
               >
                 {/* <Image src={urlFor(item.images[0]).url()} className="" loading="lazy" alt={item._id} height={270} width={250} /> */}
-                <div className="group flex justify-center text-center relative overflow-hidden cursor-pointer">
+                <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
                   <Image
                     src={urlFor(item.images[0]).url()}
                     alt="An image"
                     height={270}
                     width={250}
-                    className="rounded-md  hover:object-top object-cover ease-in-out duration-500 group-hover:rotate-6 group-hover:scale-125"
+                    className="aspect-square rounded-xl bg-gray-100 relative"
                   />
-                  <div className="absolute hover:bg-black/50  w-full h-full opacity-50 transition-opacity duration-500 group-hover:opacity-70" />
                 </div>
 
                 <h1 className="Allproduct-name">{item.name}</h1>
